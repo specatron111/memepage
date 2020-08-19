493e7fc1cef462d0c338738b06ecc9bd2fbf9240
specatron111 <61760656+specatron111@users.noreply.github.com>
2020-08-19 17:01:41 +0100

    FIXED VIDEO AUDIO

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from instabot import Bot
 import os, os.path
 import re
 import requests,urllib
-import praw
+import praw,ffmpeg
 import configparser
 import concurrent.futures
 import argparse
@@ -20,7 +20,7 @@ class redditImageScraper: #SCRAPE IMAGES
         self.sub = sub
         self.limit = limit
         self.order = order
-        self.path = f'home/ec2-user/memepage/images/{self.sub}/'
+        self.path = f'images/{self.sub}/'
         self.reddit = praw.Reddit(client_id=config['REDDIT']['client_id'],
                                   client_secret=config['REDDIT']['client_secret'],
                                   user_agent='Multithreaded Reddit Image Downloader v2.0 (by u/impshum)')
@@ -44,17 +44,41 @@ class redditImageScraper: #SCRAPE IMAGES
                 submissions = self.reddit.subreddit(self.sub).new(limit=None)
 
             for submission in submissions:
-                try: #VIDEO STUFF
-                    url = submission.media['reddit_video']['fallback_url']
-                    url = url.split("?")[0]
-                    fname = self.path + submission.title[:30].rstrip() + ".mp4"
-                    if not os.path.isfile(fname):
-                        urllib.request.urlretrieve(url,fname)
-                        go += 1
-                        if go >= self.limit:
-                            break
+                #try: #VIDEO STUFF
+                vidurl = submission.media['reddit_video']['fallback_url'] #obtain video part
+                audio_url = vidurl[:vidurl.rfind('/')] + '/DASH_audio.mp4' #obtain audio part (holy shit reddit documentation is shit, its dash_audio.mp4 not /audio, literal aids)
+                vidurl = vidurl.split("?")[0] #formatting?
+                try:
+                    urllib.request.urlretrieve(audio_url,self.path + "audio.mp3") #download audio
                 except:
                     pass
+                if os.path.isfile(self.path + "audio.mp3"): #IF WE ACC GOT AUDIO
+                    urllib.request.urlretrieve(vidurl,self.path + "video.mp4") #download vid
+                    infile1 = ffmpeg.input(self.path + "video.mp4") #set ffmpeg stuff
+                    infile2 = ffmpeg.input(self.path + "audio.mp3")
+
+
+                    fname = self.path + submission.title[:30].rstrip() + ".mp4"
+                    if not os.path.isfile(fname):
+                        merged  = ffmpeg.concat(infile1, infile2, v=1, a=1) #merge
+                        ffmpeg.output(merged[0], merged[1], fname) #output
+
+                        os.remove(self.path + "video.mp4")
+                        os.remove(self.path + "audio.mp3")
+                        go += 1
+                        print(go)
+                        if go >= self.limit:
+                            break
+                else:
+                    fname = self.path + submission.title[:30].rstrip() + ".mp4"
+                    if not os.path.isfile(fname):
+                        urllib.request.urlretrieve(vidurl,fname) #download without audio
+                        go += 1
+                        print(go)
+                        if go >= self.limit:
+                            break
+                #except:
+                #    pass
                 
                 if not submission.stickied and submission.url.endswith(('jpg', 'jpeg', 'png')): #photo stuff
                     if submission.url[-4:] == "jpeg": #to add correct file extension
@@ -74,7 +98,7 @@ class redditImageScraper: #SCRAPE IMAGES
 
 
 def main():
-    scraper = redditImageScraper(subreddit, 100, "top")
+    scraper = redditImageScraper(subreddit, 100, "hot")
     scraper.start() #START SCRAPER
 
 while True: #run constantly
@@ -84,7 +108,7 @@ while True: #run constantly
         main()
                 
         imgs = []
-        path = "home/ec2-user/memepage/images/"+ subreddit #PATH OF SCRAPED IMAGES
+        path = "images/"+ subreddit #PATH OF SCRAPED IMAGES
         valid_images = [".jpg",".gif",".png",".tga",".mp4"]
         for f in os.listdir(path):
             ext = os.path.splitext(f)[1]
@@ -93,19 +117,17 @@ while True: #run constantly
             imgs.append(str(os.path.join(path,f))) #ADD TO LIST
 
         imgsN = len(imgs) #get how many photos
-        timeperpost = 43200/imgsN #equal period between posting every 12 hours
+        timeperpost = 1 #4320/imgsN #equal period between posting every 12 hours
         print(timeperpost)
         for i in imgs:
             if i.endswith(".jpeg"): #to add correct file extension
                 text = i[:-5]
-                pathU = path + '/' 
-                text = text.replace(pathU,"")
+                text = text.replace(path,"")
                 bot.upload_photo(i, 
                                 caption = text + "\n \n #meme #memes #funny #dankmemes #memesdaily #funnymemes #lol #follow #dank #humor #like #love #dankmeme #tiktok #lmao #instagram #comedy #ol #anime #fun #dailymemes #memepage #edgymemes #offensivememes #memestagram #funnymeme #memer #fortnite #instagood #bhfyp") #UPLOAD LIST OF PHOTOS
             elif i.endswith(".mp4"): #to add correct file extension
                 text = i[:-4]
-                pathU = path + '/' 
-                text = text.replace(pathU,"")
+                text = text.replace(path,"")
                 try:
                     bot.upload_video(i, 
                                     caption = text + "\n \n #meme #memes #funny #dankmemes #memesdaily #funnymemes #lol #follow #dank #humor #like #love #dankmeme #tiktok #lmao #instagram #comedy #ol #anime #fun #dailymemes #memepage #edgymemes #offensivememes #memestagram #funnymeme #memer #fortnite #instagood #bhfyp") #UPLOAD LIST OF PHOTOS
@@ -115,8 +137,7 @@ while True: #run constantly
 
             else:
                 text = i[:-4]
-                pathU = path + '/' #bypass EOL error lmfao
-                text = text.replace(pathU,"")
+                text = text.replace(path,"")
                 bot.upload_photo(i, 
                                 caption = text + "\n \n #meme #memes #funny #dankmemes #memesdaily #funnymemes #lol #follow #dank #humor #like #love #dankmeme #tiktok #lmao #instagram #comedy #ol #anime #fun #dailymemes #memepage #edgymemes #offensivememes #memestagram #funnymeme #memer #fortnite #instagood #bhfyp") #UPLOAD LIST OF PHOTOS
                 
